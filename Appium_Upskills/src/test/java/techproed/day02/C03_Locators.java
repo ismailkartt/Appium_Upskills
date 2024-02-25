fcf954abe5a45e72396626f3f24747c17cb54a56
Ismail Kart <ismailkartt@gmail.com>
2024-02-25 11:22:12 +0300

    v1.5

--- Appium_Upskills/src/test/java/techproed/day02/C03_Locators.java
+++ Appium_Upskills/src/test/java/techproed/day02/C03_Locators.java
@@ -1,7 +1,9 @@
 package techproed.day02;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -31,6 +33,30 @@ public class C03_Locators {
 
         URL url = new URL("http://0.0.0.0:4723");
         AndroidDriver driver = new AndroidDriver(url,options);
+
+
+        // 1-) AccessibilityId
+        WebElement element = driver.findElement(AppiumBy.accessibilityId("Accessibility"));
+        System.out.println("Accessibility Id: " + element.getText());
+
+        // 2-) id (ResourcesId)
+        element = driver.findElements(AppiumBy.id("android:id/text1")).get(1);
+        System.out.println("Accessibility Id: " + element.getText());
+
+        // 3-) className
+        element = driver.findElements(AppiumBy.className("android.widget.TextView")).get(2);
+        System.out.println("ClassName : " + element.getText());
+
+        // 4-) androidUIAutamator
+        WebElement element4 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Accessibility\")"));
+        System.out.println("Android UI Automator "+ element4.getText());
+        //5- xpath
+        WebElement element5 = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@content-desc=\"Accessibility\"]"));
+        WebElement element6 = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Accessibility\"]"));
+        System.out.println("xpath content-desc:  "+ element5.getText());
+        System.out.println("xpath text:  "+ element6.getText());
+
+
 
     }
 
